c9e64e8e7a9bcd19103a16d5fff179c3720600af
CaptainFlasmr <james@dyerdwelling.family>
2023-04-11 17:14:19 +0100

    Added python version of tool for cross platform

---
+++ python/ArtRageTool_X.py
@@ -0,0 +1,232 @@
+import tkinter as tk
+import tkinter.font as tkFont
+import pyautogui
+import time
+
+clickx=1000
+clicky=30
+
+class App:
+    def __init__(self, root):
+#        tk.Tk.__init__(self)
+        var = tk.IntVar()
+        tk.Tk.withdraw(root)
+        root.floater = FloatingWindow(root)
+
+class FloatingWindow(tk.Toplevel):
+    def __init__(self, *args, **kwargs):
+        tk.Toplevel.__init__(self, *args, **kwargs)
+        fg="#dddddd"
+        bg="#222222"
+        relief="solid"
+        bw=40
+        bh=30
+        bp=4
+        bxc=bp
+        byc=bp
+        topl=15
+        width=(5*bw)+(6*bp)
+        height=(2*bh)+(4*bp)+topl
+        alignstr = '%dx%d+%d+%d' % (width, height, 500, 100)
+        self.geometry(alignstr)
+        self.attributes('-topmost',True)
+        self.attributes('-alpha',0.5)
+        self.config(bg=bg)
+        self.overrideredirect(True)
+
+        self.grip = tk.Label(self)
+        self.grip["bg"] = bg
+        self.grip.place(x=bxc,y=byc,width=(bw*4)+(bp*3),height=topl)
+        
+        self.grip.bind("<ButtonPress-1>", self.start_move)
+        self.grip.bind("<ButtonRelease-1>", self.stop_move)
+        self.grip.bind("<B1-Motion>", self.do_move)
+
+        ft = tkFont.Font(family='Helvetica',size=10)
+                
+        bxc=(bw*4)+(bp*5)
+        GButton_862=tk.Button(self)
+        GButton_862["bg"] = bg
+        GButton_862["font"] = ft
+        GButton_862["fg"] = fg
+        GButton_862["text"] = "x"
+        GButton_862["relief"] = relief
+        GButton_862.place(x=bxc,y=byc,width=bw,height=topl)
+        GButton_862["command"] = root.quit
+
+        byc+=topl+bp
+        bxc=bp
+        GButton_861=tk.Button(self)
+        GButton_861["bg"] = bg
+        GButton_861["font"] = ft
+        GButton_861["fg"] = fg
+        GButton_861["text"] = "<"
+        GButton_861["relief"] = relief
+        GButton_861.place(x=bxc,y=byc,width=bw,height=bh)
+        GButton_861["command"] = self.GButton_861_command
+
+        bxc+=bw+bp
+        GButton_729=tk.Button(self)
+        GButton_729["bg"] = bg
+        GButton_729["font"] = ft
+        GButton_729["fg"] = fg
+        GButton_729["text"] = ">"
+        GButton_729["relief"] = relief
+        GButton_729.place(x=bxc,y=byc,width=bw,height=bh)
+        GButton_729["command"] = self.GButton_729_command
+                
+        bxc+=bw+bp
+        GButton_914=tk.Button(self)
+        GButton_914["bg"] = bg
+        GButton_914["font"] = ft
+        GButton_914["fg"] = fg
+        GButton_914["text"] = "C"
+        GButton_914["relief"] = relief
+        GButton_914.place(x=bxc,y=byc,width=bw,height=bh)
+        GButton_914["command"] = self.GButton_914_command
+        
+        bxc+=bw+bp
+        GCheckBox_117=tk.Checkbutton(self, text='', variable=var)
+        GCheckBox_117["bg"] = bg
+        GCheckBox_117["font"] = ft
+        GCheckBox_117["fg"] = "#333333"
+        GCheckBox_117["text"] = ""
+        GCheckBox_117["relief"] = relief
+        GCheckBox_117.place(x=bxc,y=byc,width=bw,height=bh)
+        GCheckBox_117["offvalue"] = "0"
+        GCheckBox_117["onvalue"] = "1"
+        GCheckBox_117["command"] = self.GCheckBox_117_command
+        
+        bxc+=bw+bp
+        GButton_251=tk.Button(self)
+        GButton_251["bg"] = bg
+        GButton_251["font"] = ft
+        GButton_251["fg"] = fg
+        GButton_251["text"] = "Z"
+        GButton_251["relief"] = relief
+        GButton_251.place(x=bxc,y=byc,width=bw,height=bh)
+        GButton_251["command"] = self.GButton_251_command
+        
+        # Second LIne
+        byc+=bh+bp
+        bxc=bp
+        GButton_822=tk.Button(self)
+        GButton_822["bg"] = bg
+        GButton_822["font"] = ft
+        GButton_822["fg"] = fg
+        GButton_822["text"] = "Shift"
+        GButton_822["relief"] = relief
+        GButton_822.place(x=bxc,y=byc,width=(bw*2)+bp,height=bh)
+        GButton_822["command"] = self.GButton_822_command
+        
+        bxc+=(bw*2)+(bp*2)
+        GButton_834=tk.Button(self)
+        GButton_834["bg"] = bg
+        GButton_834["font"] = ft
+        GButton_834["fg"] = fg
+        GButton_834["text"] = "De"
+        GButton_834["relief"] = relief
+        GButton_834.place(x=bxc,y=byc,width=bw,height=bh)
+        GButton_834["command"] = self.GButton_834_command
+
+        bxc+=bw+bp
+        GButton_440=tk.Button(self)
+        GButton_440["bg"] = bg
+        GButton_440["font"] = ft
+        GButton_440["fg"] = fg
+        GButton_440["text"] = "Alt"
+        GButton_440["relief"] = relief
+        GButton_440.place(x=bxc,y=byc,width=bw,height=bh)
+        GButton_440["command"] = self.GButton_440_command
+
+        bxc+=bw+bp
+        GButton_346=tk.Button(self)
+        GButton_346["bg"] = bg
+        GButton_346["font"] = ft
+        GButton_346["fg"] = fg
+        GButton_346["text"] = "M"
+        GButton_346["relief"] = relief
+        GButton_346.place(x=bxc,y=byc,width=bw,height=bh)
+        GButton_346["command"] = self.GButton_346_command
+
+    def start_move(self, event):
+        self.x = event.x
+        self.y = event.y
+
+    def stop_move(self, event):
+        self.x = None
+        self.y = None
+
+    def do_move(self, event):
+        deltax = event.x - self.x
+        deltay = event.y - self.y
+        x = self.winfo_x() + deltax
+        y = self.winfo_y() + deltay
+        self.geometry(f"+{x}+{y}")
+
+    def GButton_861_command(self):
+        print("<")
+        pyautogui.click(clickx,clicky)
+        pyautogui.hotkey('ctrl', 'z')
+        
+    def GButton_729_command(self):
+        print(">")
+        pyautogui.click(clickx,clicky)
+        pyautogui.hotkey('ctrl', 'y')
+        
+    def GButton_914_command(self):
+        print("C")
+        pyautogui.click(clickx,clicky)
+        pyautogui.hotkey('ctrl', 'shift', "c")
+
+    def GCheckBox_117_command(self):
+        print("Flip")
+        pyautogui.click(clickx,clicky)
+
+        if var.get() == 1:
+            print(1)
+            pyautogui.keyDown('h')
+        else:
+            print(0)
+            pyautogui.keyUp('h')
+        
+    def GButton_251_command(self):
+        print("Z")
+        pyautogui.click(clickx,clicky)
+        pyautogui.keyDown('space')
+        pyautogui.keyDown('shift')
+        time.sleep(1.5)
+        pyautogui.keyUp('space')
+        pyautogui.keyUp('shift')
+
+    def GButton_822_command(self):
+        print("Shift")
+        pyautogui.click(clickx,clicky)
+        pyautogui.keyDown('shift')
+        time.sleep(1.5)
+        pyautogui.keyUp('shift')
+
+    def GButton_834_command(self):
+        print("Des")
+        pyautogui.click(clickx,clicky)
+        pyautogui.hotkey('ctrl', 'd')
+
+    def GButton_440_command(self):
+        print("Alt")
+        pyautogui.click(clickx,clicky)
+        pyautogui.keyDown('alt')
+        time.sleep(1.5)
+        pyautogui.keyUp('alt')
+
+    def GButton_346_command(self):
+        print("M")
+        pyautogui.click(clickx,clicky)
+        pyautogui.keyDown('space')
+        time.sleep(1.5)
+        pyautogui.keyUp('space')
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    var = tk.IntVar()
+    app=App(root)
+    root.mainloop()
